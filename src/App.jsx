a8aca1b83b4fea81a3c6c07d671edf7b5c6e0e51
NikitaGolitsyn <nikita.golitsyn@wesoftyou.com>
2022-04-10 21:28:47 +0300

    Authorization, dependencies build

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => (
+  <h1>
+    A man can have anything if he is willing to sacrifice. With your birth comes
+    a solemn vow: You will have nothing! Your privilage is the dirt! In the
+    darkness, only ambitions will guid you.
+  </h1>
+);
+
+export default App;
